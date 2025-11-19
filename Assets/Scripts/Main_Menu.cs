faec10c7f68bc7d8d75f7b9a03efb66957c14864
MuhammedAbdussamed <muhammedabdussamed2006@gmail.com>
2025-11-19 13:27:36 +0300

    Initial commit

---
+++ Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+    }
+    
+    public void OpenGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
